ef5b98617b739f927b1300078863939a1c91702a
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-08-20 11:34:06 +0500

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-restricted-globals, prefer-destructuring */
+/* eslint-disable no-restricted-globals, prefer-destructuring, no-unresolved */
 import './style.css';
 import todoStatusUpdate from './todoStatusUpdate.js';
 import dragDrop from './dragDrop.js';
@@ -86,4 +86,4 @@ const edit = (tasks) => {
   }
 };
 edit(tasks);
-/* eslint-enable no-restricted-globals, prefer-destructuring */
+/* eslint-enable no-restricted-globals, prefer-destructuring, no-unresolved */
